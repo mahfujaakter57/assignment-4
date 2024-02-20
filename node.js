72a8a85a9ee4680ec3105cf361c7acc8419d00a2
Mahfuja Akter <mahfujaakter057@gmail.com>
2024-02-20 16:22:46 +0600

    first commit

---
+++ node.js
@@ -0,0 +1,26 @@
+// 
+let book = 8 ;
+let pen = 10;
+let khata = 5;
+
+if(pen >= khata){
+    console.log('ami kinbo');
+    
+}
+else{
+    console.log('ami kinbo na');
+}
+// 
+
+
+var mobile = 30000;
+var myBudget = 50000;
+
+if(myBudget <= mobile){
+    console.log(' ami mobile kibo na');
+}else if(myBudget >= mobile){
+    console.log('ami mobile kinbo');
+}else{
+    console.log('amr budget a hone na ');
+}
+
